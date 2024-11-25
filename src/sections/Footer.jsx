c4af5bbdfb758c04fafba4926b751f7c7e81edd7
phoenix75-S7F <sfrancoff@gmail.com>
2024-11-25 16:35:15 -0300

    about page

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,17 +3,17 @@ import { socials } from "../constants/index.jsx";
 const Footer = () => {
 	return (
 		<footer>
-			<div className="container -mt-24 py-12">
+			<div className="container py-12">
 				<div className="flex w-full max-md:flex-col">
 					
 					<div className="flex items-center justify-center sm:ml-auto ">
-						<p className="legal-after relative mr-10 text-p5 transition-all duration-500 hover:text-p1">
+						<p className="legal-after relative mr-10 text-p5 transition-all duration-1000 hover:text-p1">
 							Politica de Privacidade
 						</p>
 						<p className="legal-after relative mr-10 text-p5 transition-all duration-500 hover:text-p1">
 							Termos de Uso
 						</p>
-						<p className="text-p5 transition-all duration-500 hover:text-p1">
+						<p className="text-p5 transition-all duration-500 hover:text-p1 cursor-pointer">
 							Sobre a Empresa
 						</p>
 					</div>					
@@ -34,7 +34,7 @@ const Footer = () => {
 
 					
 				</div>
-				<div className="flex flex-1 flex-wrap items-center justify-center gap-5 py-12">
+				<div className="flex flex-1 flex-wrap items-center justify-center gap-5 py-8">
 					<p className="opacity-85 text-p3 font-semibold">Ponto Zero © 2024 Todos os Direitos Reservados.</p>
 				</div>
 			</div>
