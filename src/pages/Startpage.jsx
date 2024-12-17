2a249848f56288c0e85c857a2706d9b4b0c1697a
phoenix75-S7F <sfrancoff@gmail.com>
2024-12-17 10:36:48 -0300

    parting line

--- src/pages/Startpage.jsx
+++ src/pages/Startpage.jsx
@@ -6,7 +6,7 @@ const Startpage = () => {
     <main className="flex min-h-screen w-full justify-between font-inter z-10" >
       <Start />
       <div className="auth-asset">
-        <div className="absolute bg-slate-400 opacity-100 top-[0%] left-[-7%] 2md:left[21%] right-0 h-full w-[1px] z-10"/>
+        <div className="absolute bg-slate-400 opacity-100 top-[0%] left-[-7%] right-0 h-full w-[1px] z-10"/>
         <div>
           <img
             src="/images/screen.jpg"
