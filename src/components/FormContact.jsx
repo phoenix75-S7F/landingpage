eabd264794dc59289b3a33f71085cc60addbb101
phoenix75-S7F <sfrancoff@gmail.com>
2025-01-01 15:01:33 -0300

    fix

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -94,9 +94,7 @@ const FormContact = () => {
               </label>
               <div className="mb-4">
                 <input
-                  id="fone"
-                  name="fone"
-                  type="fone"
+                  
                   placeholder="(00) 00000-0000"
                   autoComplete="fone"
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
